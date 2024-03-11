597d898d25e33cd361a76e5f552cda69886b3de0
amariop2685 <acostap.marioa@gmail.com>
2024-03-10 22:28:42 -0300

    corrigiendo login.jsx

--- src/components/pages/Login.jsx
+++ src/components/pages/Login.jsx
@@ -19,32 +19,6 @@ function Login() {
   };
 
   return (
-
-<>
-    <div>
-  <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet" type="text/css" />
-  <div className="login">
-    <h2 className="active"> sign in </h2>
-    <h2 className="nonactive"> sign up </h2>
-    <form>
-      <input type="text" className="text" name="username" />
-      <span>username</span>
-      <br />
-      <br />
-      <input type="password" className="text" name="password" />
-      <span>password</span>
-      <br />
-      <input type="checkbox" id="checkbox-1-1" className="custom-checkbox" />
-      <label htmlFor="checkbox-1-1">Keep me Signed in</label>
-      <button className="signin">
-        Sign In
-      </button>
-      <hr />
-      <a href="#">Forgot Password?</a>
-    </form>
-  </div>
-</div>
-
     
     <div className='d-flex justify-content-center align-items-center vh-100'>
     <div className="row text-center">
@@ -77,7 +51,7 @@ function Login() {
 
     </div>
   </div>
-</>
+
   );
 }
 
--- src/services/Auth.services.js
+++ src/services/Auth.services.js
@@ -1,2 +1,10 @@
 import axios from 'axios';
 
+const WS_path = "https://api-ecommerce-express-6nv9.onrender.com";
+
+
+const loginSerivce = async (data) => {
+    console.log('Entro a loginService');
+};
+
+export {loginSerivce};
